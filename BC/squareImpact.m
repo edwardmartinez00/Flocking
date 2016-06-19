81fc0b92e12b7fb91c82cca2443e9552ff0eca38
hottieScotty <samahan@asu.edu>
2016-06-19 03:58:12 +0000

    Add files via upload

--- BC/squareImpact.m
+++ BC/squareImpact.m
@@ -23,5 +23,32 @@ function dV = squareImpact(X,V,P)
     r_star = sqrt(sum(D_star.^2,2));
     r_star = r_star*[1 1];    
     
-    dV = dV - w(r_star,P).*(D_star./r_star); 
+    dV = dV - w(r_star,P).*(D_star./r_star);
+    
+    % calculate distances between dog and each bird for repulsion purposes
+    if (P.DogExternal)
+        % calculate distance between dog and each bird
+        Dog_X = ones(P.N,1)*P.X_dog;
+        Mat_Dog = Dog_X-X;
+        Dog_D = sqrt((Mat_Dog(:,1)).^2+(Mat_Dog(:,2)).^2);
+        
+        % ensure that the dog does not follow or repel itself
+        M = max(Dog_D);
+        Dog_D(1,:) = max(M,P.d)+1;
+    elseif (P.DogInternal)
+        % calculate distance between dog and each bird
+        Dog_X = ones(P.N,1)*X(1,:);
+        Mat_Dog = Dog_X-X;
+        Dog_D = sqrt((Mat_Dog(:,1)).^2+(Mat_Dog(:,2)).^2);
+        
+        % ensure that the dog does not follow or repel itself
+        M = max(Dog_D);
+        Dog_D(1,:) = max(M,P.d)+1;
+    end
+    
+    % if there is a dog in the model, repel the birds
+    if (P.Dog)
+        Dog_D = Dog_D*ones(1,2);
+        dV = dV-P.sD.*w_dog(Dog_D,P).*(Mat_Dog./Dog_D);
+    end
 end
