63cdf58d1cdb0b94f55bcd3f1b61ce81ecd5650f
Edward Martinez <edwardmartinez00@gmail.com>
2016-06-20 11:19:59 -0700

    Add files via upload

--- BC/squareReflex.m
+++ BC/squareReflex.m
@@ -16,8 +16,11 @@
 function [X_new,V_new] = squareReflex(X,V,X_new,V_new,P)
     absorbancy = 2 - P.absorb;
     
-    % calculate point on boundary in front of bird (point of impact)
-    [X_star,dt_star] = sqboundaryImpact(X,V,P);
+%     % calculate point on boundary in front of bird (point of impact)
+%     [X_star,dt_star] = sqboundaryImpact(X,V,P);
+%     
+%     % Allocating space
+%     X_starstar = zeros(P.N,2);
     
     for i=1:P.N     % Bird loop
         % Checking if bounds violated
@@ -26,16 +29,37 @@ function [X_new,V_new] = squareReflex(X,V,X_new,V_new,P)
 %         
 %             if (X_star(i,1)>=X_star(i,2)) && (X_star(i,1)>-X_star(i,2))
 %                 normal = [1 0];     % East wall
-%             elseif (X_star(i,1)<=-X_star(i,2)) && (X_star(i,1)>X_star(i,2))
+%             end
+%             if (X_star(i,1)<=-X_star(i,2)) && (X_star(i,1)>X_star(i,2))
 %                 normal = [0 -1];    % South wall
-%             elseif (X_star(i,1)<=X_star(i,2)) && (X_star(i,1)<-X_star(i,2))
+%             end
+%             if (X_star(i,1)<=X_star(i,2)) && (X_star(i,1)<-X_star(i,2))
 %                 normal = [-1 0];    % West wall
-%             else
+%             end
+%             if (X_star(i,1)>=-X_star(i,2)) && (X_star(i,1)<X_star(i,2))
 %                 normal = [0 1];     % North wall
 %             end
 %             
 %             V_new(i,:) = V(i,:)-absorbancy*dot(V(i,:),normal)*normal;
-%             X_new(i,:) = X_star(i,:)+(P.dt-dt_star(i,:)).*V_new(i,:); 
+%             X_new(i,:) = X_star(i,:)+(P.dt-dt_star(i,:)).*V_new(i,:);
+%             
+%             [~,dt_starstar] = sqboundaryImpact(X,V,P);
+%             if dt_starstar(i,:) < (P.dt-dt_star)
+%                 v_star = V_new(i,:);
+%                 x_starstar = X_star(i,:) + (dt_starstar(i,:)-dt_star(i,:)).*V_new(i,:);
+%             
+%                 if (x_starstar(1)>=x_starstar(2)) && (x_starstar(1)>-x_starstar(2))
+%                     normal = [1 0];     % East wall
+%                 elseif (x_starstar(1)<=-x_starstar(2)) && (x_starstar(1)>x_starstar(2))
+%                     normal = [0 -1];    % South wall
+%                 elseif (x_starstar(1)<=x_starstar(2)) && (x_starstar(1)<-x_starstar(2))
+%                     normal = [-1 0];    % West wall
+%                 else
+%                     normal = [0 1];     % North wall
+%                 end
+%                 V_new(i,:) = v_star-absorbancy*dot(v_star,normal)*normal;
+%                 X_new(i,:) = x_starstar + (P.dt-dt_starstar(i,:)).*V_new(i,:);
+%             end
 %         end  
         if X_new(i,1) > P.L
             dt_star = (P.L-X(i,1))/(V(i,1)+1e-10);
@@ -56,12 +80,40 @@ function [X_new,V_new] = squareReflex(X,V,X_new,V_new,P)
         else
             change = 0;
         end
-        
+
         % Updating position and velocity
         if change==1 % Outside boundary
             V_new(i,:) = V(i,:)-absorbancy*dot(V(i,:),normal)*normal;
             x_star = X(i,:)+dt_star*V(i,:);
             X_new(i,:) = x_star+(P.dt-dt_star)*V_new(i,:);
+            
+            rounding = 10;
+            if round(X_new(i,1),rounding) > P.L
+                dt_starstar = (P.L-X(i,1))/(V(i,1)+realmin);
+                normal = [1,0];
+                change = 1;
+            elseif round(X_new(i,2),rounding) > P.L
+                dt_starstar = (P.L-X(i,2))/(V(i,2)+realmin);
+                normal = [0,1];
+                change = 1;
+            elseif round(X_new(i,1),rounding) < -P.L
+                dt_starstar = (-P.L-X(i,1))/(V(i,1)+realmin);
+                normal = [-1,0];
+                change = 1;
+            elseif round(X_new(i,2),rounding) < -P.L 
+                dt_starstar = (-P.L-X(i,2))/(V(i,2)+realmin);
+                normal = [0,-1];
+                change = 1;
+            else
+                change = 0;
+            end
+            
+            if change == 1
+                v_star = V_new(i,:);
+                x_starstar = x_star + (dt_starstar-dt_star).*V_new(i,:);
+                V_new(i,:) = v_star-absorbancy*dot(v_star,normal)*normal;
+                X_new(i,:) = x_starstar + (P.dt-dt_starstar).*V_new(i,:);
+            end
         end
     end
 end
